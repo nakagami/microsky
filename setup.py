f853e2a264e525dc4da2ce32d4f55c7f69bfb02f
Hajime Nakagami <nakagami@gmail.com>
2025-01-01 10:00:44 +0900

    add long_description

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='microsky',
       version=__import__('microsky').__version__,
       description='Simple bsky.app client',
+      long_description=open('README.rst').read(),
       url='https://github.com/nakagami/microsky',
       author='Hajime Nakagami',
       author_email='nakagami@gmail.com',
